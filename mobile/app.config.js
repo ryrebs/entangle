239cc7d09619363ab8aea04e9f0a993e16858c32
rodel <rodelrebucas@gmail.com>
2020-08-24 01:37:38 +0800

    fix config not found

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -5,17 +5,22 @@ const extractEnv = () => {
     return {
       EXPO_API: process.env.EXPO_LOCAL_API,
       EXPO_ENV: process.env.EXPO_ENV,
+      EXPO_CLIENT_API: process.env.EXPO_DEV_CLIENT_API,
     };
   } else {
     return {
       EXPO_API: process.env.EXPO_PROD_API,
       EXPO_ENV: process.env.EXPO_ENV,
+      EXPO_CLIENT_API: process.env.EXPO_PROD_CLIENT_API,
     };
   }
 };
 
-export default {
-  name: "Entangle",
-  version: "1.0.0",
-  extra: extractEnv(),
-};
+export default ({ config }) => ({
+  ...config,
+  ...{
+    name: "Entangle",
+    version: "1.0.0",
+    extra: extractEnv(),
+  },
+});
