5cb7e9d589d903490dd81b30c775100e59185427
rodel <rodelrebucas@gmail.com>
2020-09-07 03:30:50 +0800

    refactor store

--- mobile/src/index.tsx
+++ mobile/src/index.tsx
@@ -2,28 +2,27 @@ import React from "react";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import Main from "./screens";
-import setupStore from "./store/index";
+import setupStore, { store, persistor } from "./store/index";
 import setUpApi from "./service/api";
 import "./utils/i18n";
 import { enableScreens } from "react-native-screens";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import ThemeProvider from "./context/ThemeContextProvider";
 
-const { store, persistor } = setupStore();
+setupStore();
 enableScreens();
 setUpApi();
 
 const App = () => (
   <ThemeProvider>
     <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <SafeAreaProvider>
-            <Main />
-          </SafeAreaProvider>
-        </PersistGate>
+      <PersistGate loading={null} persistor={persistor}>
+        <SafeAreaProvider>
+          <Main />
+        </SafeAreaProvider>
+      </PersistGate>
     </Provider>
   </ThemeProvider>
 );
 
-export { store };
 export default App;
